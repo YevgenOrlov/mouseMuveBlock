30fea265323ba816b29788a07b1fe792434170e9
YevgenOrlov <101797140+YevgenOrlov@users.noreply.github.com>
2022-08-21 12:14:17 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,25 @@
+// let block = document.getElementById("one");
+// function randint(min, max) {
+//   let rand = min + Math.random() * (max + 1 - min);
+//   return Math.floor(rand);
+// }
+
+// let a = 0;
+// block.onmousemove = function () {
+//   this.style.left = 10 + a + "px";
+//   a++;
+
+//   this.style.background = `rgb(${randint(0, 255)},  ${randint(
+//     0,
+//     255
+//   )}, ${randint(0, 255)})`;
+// };let
+
+let f = 6;
+let d = 2;
+// [f, d] = [d, f];
+// f = [d, (d = f)][0];
+console.log(f=[d, (d = f)][0]);
+console.log('f=', f.d,'d=', d);
+
+
